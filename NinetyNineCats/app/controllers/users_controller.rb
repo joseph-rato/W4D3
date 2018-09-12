54e5bdb2605286b97628b68311a5b1e37d20a9b3
joseph-rato <joseph.rato@gmail.com>
2018-09-12 14:36:43 -0700

    implement user view and fix syntax

--- NinetyNineCats/app/controllers/users_controller.rb
+++ NinetyNineCats/app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController 
   
   def new
+    @user = User.new
     render :new 
   end 
   
+  # come back later to check on this stuff
   def create
-    @user = User.find_by(params[:id])
+    @user = User.new(user_params)
     
-    if 
+    if @user.save
+      redirect_to users_url 
+    else 
+      render flash.now[:errors]
+      # render json: @user.errors.full_messages, status: 422
+    end
+  end
+  
+  private 
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- NinetyNineCats/app/models/user.rb
+++ NinetyNineCats/app/models/user.rb
@@ -13,6 +13,7 @@
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :session_token, uniqueness: true 
+  validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :reset_session_token
   
   attr_reader :password
